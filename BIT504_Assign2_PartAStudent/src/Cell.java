6aa04c09a6f519aaaf64d3728caa473a019c4ea1
NickQZ <nic.ejones25@gmail.com>
2024-04-11 11:14:19 +1200

    Set content inside Clear cell to Empty, this means that the cell is ready to be played.
    V.1.8

--- BIT504_Assign2_PartAStudent/src/Cell.java
+++ BIT504_Assign2_PartAStudent/src/Cell.java
@@ -17,7 +17,7 @@ public class Cell {
 		this.row = row;
 		this.col = col;
 		
-		//TODO: call the method that sets the cell content to EMPTY
+		// Sets the cell content to EMPTY
 		clear();
 	}
 
@@ -46,8 +46,9 @@ public class Cell {
 	/** Set this cell's content to EMPTY */
 	public void clear() {
 
-		// TODO: Set the value of content to Empty (Remember this is an enum)
-
+		// Set content inside Clear cell to Empty, this means that the cell is ready to be played.
+		content = Player.Empty;
+		
 	}
 
 }
